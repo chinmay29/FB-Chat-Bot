0f8ec8004da347d832b5d54634dadd69b2c3201f
Chinmay Deshpande <cadeshpa@indiana.edu>
2016-04-21 23:22:54 -0400

    hello

--- index.js
+++ index.js
@@ -43,3 +43,24 @@ app.post('/webhook/', function (req, res) {
 })
 
 var token = "EAAWwZC1IPifwBAEUBiMt1vxoOSNuRxTqx5vbPqn7MWJF1QXikCtbXQMnOGbOztDxRKZBsbwXZCwXma82lLDnVI1eOZAGZC1KEENqZAbZC3UbvNLnZAOxSrbXsviw1rsyaX8LJCoWK3QN62G4lCwH3DXvKuB7N4QAI7hZBNXLOm4O1eQZDZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
